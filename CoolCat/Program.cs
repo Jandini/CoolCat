56808bec78e7de31e53ad97a1008b2f2b279be60
Matt Janda <matt.janda.kingston@gmail.com>
2020-12-19 12:17:00 +0000

    feat: Add cool cat

---
+++ CoolCat/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace CoolCat
+{
+    class Program
+    {
+
+        static void DrawCat(string line, bool isLast = false)
+        {
+
+            foreach (var character in line)
+            {
+                var color = character switch
+                {
+                    'b' => ConsoleColor.Black,
+                    'y' => ConsoleColor.DarkYellow,
+                    'w' => ConsoleColor.White,
+                    _ => ConsoleColor.Gray,
+                };
+
+                Console.BackgroundColor = color;
+                Console.Write("  ");
+            }
+
+            if (!isLast)
+                Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+
+            DrawCat("  bb          bb  ");
+            DrawCat("  byb        byb  ");
+            DrawCat("  bbyb      bybb  ");
+            DrawCat("  b byb    byb b  ");
+            DrawCat("  b byb    byb b  ");
+            DrawCat("  b byyb  byyb b  ");
+            DrawCat("  b byyybbyyyb b  ");
+            DrawCat("  b byyyyyyyyb b  ");
+            DrawCat("  bbyyyyyyyyyybb  ");
+            DrawCat("  byyyyyyyyyyyyb  ");
+            DrawCat("  byyywwyywwyyyb  ");
+            DrawCat("  bbbbbbyybbbbbb  ");
+            DrawCat("  bbbbbbbbbbbbbb  ");
+            DrawCat("  bbbbbbwwbbbbbb  ");
+            DrawCat("  bybbbwwwwbbbyb  ");
+            DrawCat("  byyyywbbwyyyyb  ");
+            DrawCat(" bwwwwwwbbwwwwwwb ");
+            DrawCat("bwwwwwwbwwbwwwwwwb");
+            DrawCat("bbbbwwwwbbwwwbbbbb");
+            DrawCat("  bybbbbbbbbbbyb  ");
+            DrawCat(" byyy        yyyb ");
+            DrawCat("byyyy        yyyyb");
+            DrawCat("byyby        ybyyb");
+            DrawCat(" byby        ybyb ");
+            DrawCat("  bbyy      yybb  ");
+            DrawCat("   byybbbbbbyyb   ");
+            DrawCat("  byyb      byyb  ");
+            DrawCat(" b  b        b  b ");
+            DrawCat(" bbbb        bbbb ");
+            DrawCat("                  ", true);
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
